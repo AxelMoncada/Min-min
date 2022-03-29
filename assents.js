533ecab4ed1e16e84958bafec30cfc7c79370a08
Axel Moncada <51040327+AxelMoncada@users.noreply.github.com>
2022-03-29 01:05:36 -0500

    Cronometro segundo tiempo y live server

--- assents.js
+++ assents.js
@@ -1,22 +1,10 @@
 
-// Funcion prueba generador textos 
-
-
-
-
-
-
-
 // Funcion cronometro primer tiempo
 function init () { 
  var tiempo1 =document.getElementById('contador1tiempo');
 var star1tiempo= document.getElementById('btnIniciar1');
 var stop1tiempo= document.getElementById('btnPausar1');
 var reiniciar1tiempo= document.getElementById('btnReiniciar1');
-
-
-var generarmin= document.getElementById('generartxt');
-
 
 
 
@@ -56,38 +44,147 @@ reiniciar1tiempo.onclick = function () {
     sec1 = 0 ; min1 = 0 ; hrs1 = 0;
 }
 
-
-generarmin.addEventListener('click', imprimirmin);
-
-    function imprimirmin() {
-
-        var rivales =document.getElementById("rivalestxt").value;
-         var jugadatxt = document.getElementById("jugadatxt").value;
-       var  jugada= jugadatxt.replaceAll("\n", "<br>");
-       
-          var hastags = document.getElementById("hastagstxt").value;
-
-           var textogenerado = document.getElementById("textogenerado");
-
-           var  minutos1 = min1 ; 
-
-           if ( sec1 >= 30){
-            var minutosfinal = minutos1 + 1;
-          
-        }else{
-            var minutosfinal = minutos1;
-            
-        }
-        console.log(minutosfinal)
-              
-              var txtgeneradomin = rivales + "<br>"+"<br>"+ "|" + minutosfinal +"'"+ " ⏱|" + jugada + "<br>" +"<br>" +  hastags ;
-              document.getElementById("textogenerado").innerHTML= txtgeneradomin ;
-               
+        
+  // Funcionalidad generar todo el texto 
+  var generarmin= document.getElementById('generartxt');
+  generarmin.addEventListener('click', imprimirmin);
+  
+  function imprimirmin() {
+  
+  var rivales =document.getElementById("rivalestxt").value;
+   var jugadatxt = document.getElementById("jugadatxt").value;
+  var  jugada= jugadatxt.replaceAll("\n", "<br>");    
+  var hastags = document.getElementById("hastagstxt").value;
+  var textogenerado = document.getElementById("textogenerado");  
+  
+  
+  
+  // Aproximacion del minuto
+  
     
+  var minutos1 = min1
+        
+      if ( sec1 >= 30){
+          minutosfinal = minutos1 + 1;    
+             
+      }else{
+          minutosfinal = minutos1;
+           }
+
+
+            
+  var txtgeneradomin = rivales + "<br>"+"<br>"+ "|" + minutosfinal +"'"+ " ⏱|" + jugada + "<br>" +"<br>" +  hastags ;
+  document.getElementById("textogenerado").innerHTML= txtgeneradomin ;
+  
+ 
+  }
+     
+  
+            
+  
+ 
+ 
+    
+
+    
+
+
+
+
+}
+
+
+
+// cronometro segundo tiempo
+    function init2() { 
+        var tiempo2 =document.getElementById('contador2tiempo');
+       var star2tiempo= document.getElementById('btnIniciar2');
+       var stop2tiempo= document.getElementById('btnPausar2');
+       var reiniciar2tiempo= document.getElementById('btnReiniciar2');
+       
+       
+       var generarmin2= document.getElementById('generartxt');
+       var sec2 =0;
+       var min2 =45;
+       var hrs2 = 0;
+       var t2;
+       function reloj2 () {
+           sec2 ++;
+           if (sec2 >= 60){
+               sec2 = 0;
+               min2 ++;
+               if(min2 >= 60){
+                   min2=0;
+                   hrs2 ++; 
+               }
+           }
+       }
+       function add2() {
+           reloj2();
+           tiempo2.textContent = (hrs2 > 9 ?  hrs2  : "0" + hrs2)
+                                       + ":" + (min2 > 9 ? min2 : "0" + min2)
+                                       + ":" + (sec2 > 9 ? sec2 : "0"+ sec2);
+                                       timer2();
+       }
+       function timer2 ( ) {
+           t2 =setTimeout (add2, 1000);
+       }
+       timer2();
+       
+       star2tiempo.onclick = timer2 ;
+       stop2tiempo.onclick = function(){
+           clearTimeout(t2);
+       }
+       reiniciar2tiempo.onclick = function () {
+           tiempo2.textContent = "00:45:00";
+           sec2 = 0 ; min2 = 45 ; hrs2 = 0;
+       }
+
+
+      
+        // Funcionalidad generar todo el texto 
+  var generarmin= document.getElementById('generartxt');
+  generarmin.addEventListener('click', imprimirmin);
+  
+  function imprimirmin() {
+  
+  var rivales =document.getElementById("rivalestxt").value;
+   var jugadatxt = document.getElementById("jugadatxt").value;
+  var  jugada= jugadatxt.replaceAll("\n", "<br>");    
+  var hastags = document.getElementById("hastagstxt").value;
+  var textogenerado = document.getElementById("textogenerado");  
+  
+  
+  
+  // Aproximacion del minuto
+  
+    
+  var minutos2 = min2
+        
+      if ( sec2 >= 30){
+          minutosfinal = minutos2 + 1;    
+             
+      }else{
+          minutosfinal = minutos2;
+           }
+
+
+            
+  var txtgeneradomin = rivales + "<br>"+"<br>"+ "|" + minutosfinal +"'"+ " ⏱|" + jugada + "<br>" +"<br>" +  hastags ;
+  document.getElementById("textogenerado").innerHTML= txtgeneradomin ;
+   
+        
+       
     }
 
-    
-} 
+    // Funcion imprimir textos
+
+  
+
+
+
+// Funcion Boton copiar texto
+
 function copyText() {
     /* Get the text field */
     var copyText = document.getElementById("textogenerado");
@@ -101,11 +198,23 @@ function copyText() {
   /* Alert the copied text */
 document.getElementById("respuestatxt").innerText ="Texto copiado"
 }
-    // Funcionalidad generar todo el texto 
+    
 
-  
+  // funcion bloquear anuncios
+   
+    } 
 
-
-
-
-
+    window.onload = function(){
+        var button = document.getElementsByName("sandbox")[0]
+        var iframe = document.getElementsByName("framez")[0]
+        button.addEventListener('click',sndbx,false);
+    
+        function sndbx(){
+        var nibba = document.getElementById("framez").src;
+        if(iframe.sandbox == 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation'){
+        document.getElementById("framez").removeAttribute("sandbox"); 
+        }
+        frames['framez'].location.href=nibba;
+        iframe.sandbox = 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation';
+        }
+    }
